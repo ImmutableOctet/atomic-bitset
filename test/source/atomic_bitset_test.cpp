378d2c93b9673fcfcf85cd3e88ad90507a2186a0
Anthony Diamond <sonickid8@live.com>
2023-06-29 01:47:42 -0600

    Added `Simultaneous access` test.

--- test/source/atomic_bitset_test.cpp
+++ test/source/atomic_bitset_test.cpp
@@ -2,6 +2,8 @@
 
 #include <immutableoctet/atomic_bitset/atomic_bitset.hpp>
 
+#include <thread>
+
 #include <cstddef>
 #include <cstdint>
 
@@ -33,5 +35,61 @@ TEST_CASE("immutableoctet::atomic_bitset", "[atomic-bitset]")
 		}
 
 		REQUIRE(sum_of_bits == 98);
+	}
+
+	SECTION("Simultaneous access")
+	{
+		auto bitset = bitset_t {};
+
+		std::size_t n_elements = 4096 * 64; // 512
+
+		auto work = [&bitset](std::size_t n_elements, std::size_t stride, std::size_t offset={})
+		{
+			for (std::size_t index = offset; index < n_elements; index += stride)
+			{
+				bitset[index] = true;
+			}
+		};
+
+		{
+			auto first = std::jthread
+			{
+				work,
+
+				n_elements,
+
+				static_cast<std::size_t>(3),
+				static_cast<std::size_t>(0)
+			};
+
+			auto second = std::jthread
+			{
+				work,
+
+				n_elements,
+
+				static_cast<std::size_t>(3),
+				static_cast<std::size_t>(1)
+			};
+
+			auto third = std::jthread
+			{
+				work,
+
+				n_elements,
+
+				static_cast<std::size_t>(3),
+				static_cast<std::size_t>(2)
+			};
+		}
+
+		std::size_t sum_of_bits = 0;
+
+		for (const bool bit : bitset)
+		{
+			sum_of_bits += static_cast<std::size_t>(bit);
+		}
+
+		REQUIRE(sum_of_bits == n_elements);
 	}
 }
